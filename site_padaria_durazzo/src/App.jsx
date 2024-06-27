674c8a97b9762c05db3aeaa81ad759d3688161cd
Alexandre Durazzo <145718483+victo-alt@users.noreply.github.com>
2024-06-27 12:04:23 -0300

    Criação do site em React
    Criação da biblioteca React e inicio da estilização da Navbar

---
+++ site_padaria_durazzo/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css';
+import Navbar from './components/navbar';
+
+function App() {
+  return (
+    <div className="App">
+     <Navbar></Navbar>
+      <h2>Tarefa: Criar uma Sidebar e criar a estilização usando css no App.css</h2>
+    </div>
+  );
+}
+
+export default App;
---
+++ site_padaria_durazzo/src/components/navbar.jsx
@@ -0,0 +1,14 @@
+function Navbar() {
+    return (
+        <nav>
+            <div className="logo"/>
+            <h1>Padaria Durazzo</h1>
+            <div className="links">
+                <a href="#produtos">Produtos</a>
+                <a href="#pedidos">Pedidos</a>
+                <a href="#quem_somos">Quem somos</a>
+                <a href="#trabalhe_conosco">Trabalhe conosco</a>
+            </div>
+        </nav>
+    )
+} export default Navbar
